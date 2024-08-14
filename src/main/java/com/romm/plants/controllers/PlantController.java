ba80d754d614733d6a70a6d108fae1a87c9ac4a9
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-08-14 15:04:08 -0300

    feat: REST CRUD

--- src/main/java/com/romm/plants/controllers/PlantController.java
+++ src/main/java/com/romm/plants/controllers/PlantController.java
@@ -1,11 +1,54 @@
 package com.romm.plants.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.romm.plants.entities.Plant;
 import com.romm.plants.services.PlantService;
 
+@RestController
+@RequestMapping(value="")
 public class PlantController {
     
     @Autowired PlantService ps;
+
+    @GetMapping
+    public List<Plant> findAll() {
+        return ps.findAll();
+    }
+
+    @PostMapping
+    public Plant create(@RequestBody Plant plant) {
+        return ps.create(plant);
+    }
+
+    @GetMapping("/{plantId}")
+    public Plant findById(@PathVariable("plantId") Long plantId) {
+        return ps.findById(plantId);
+    }
+
+    @PutMapping("/{plantId}")
+    public Plant updateById(@RequestBody Plant plant, @PathVariable("plantId") Long plantId) {
+        plant.setId(plantId);
+        return ps.updateById(plant, plantId);
+    }
+
+    @DeleteMapping("/{plantId}")
+    public ResponseEntity<?> deleteById(@PathVariable("plantId") Long plantId) {
+        ps.delete(plantId);
+
+        return ResponseEntity.ok().build();
+    }
+
 
 }
--- src/main/java/com/romm/plants/services/PlantService.java
+++ src/main/java/com/romm/plants/services/PlantService.java
@@ -1,13 +1,47 @@
 package com.romm.plants.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.romm.plants.entities.Plant;
 import com.romm.plants.repositories.PlantRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class PlantService {
     
     @Autowired PlantRepository pr;
 
+    public List<Plant> findAll() {
+        return pr.findAll();
+    }
+
+    public Plant create(Plant plant) {
+        return pr.save(plant);
+    }
+
+    public Plant findById(Long id) throws EntityNotFoundException {
+        return findOrThowError(id);
+    }
+
+    public Plant updateById(Plant plant, Long id) throws EntityNotFoundException {
+        var dbPlant = findOrThowError(id);
+        if (plant.getTitle() != null) dbPlant.setTitle(plant.getTitle());
+        if (plant.getPlantingDate() != null) dbPlant.setPlantingDate(plant.getPlantingDate());
+
+        return pr.save(dbPlant);
+    }
+
+    public void delete(Long id) {
+        pr.deleteById(id);
+    }
+
+    public Plant findOrThowError (Long id) throws EntityNotFoundException {
+        var opt = pr.findById(id);
+        if (opt.isEmpty()) throw new EntityNotFoundException("There is not a plant with this id :(...");
+        return opt.get();
+    }
 }
